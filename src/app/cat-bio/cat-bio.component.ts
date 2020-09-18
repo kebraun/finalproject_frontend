b56607e2441433bb91426674c46b1f3af6517169
Kanchan <kanchankandage@gmail.com>
2020-09-18 13:13:58 -0400

    cat card fav

--- src/app/cat-bio/cat-bio.component.ts
+++ src/app/cat-bio/cat-bio.component.ts
@@ -1,37 +1,42 @@
-import { Cat } from './../interfaces/cat';
-import { CatDetailsService } from './../cat-details.service';
-import { ActivatedRoute } from '@angular/router';
-import { Component, Input, OnInit } from '@angular/core';
+import { Cat } from "./../interfaces/cat";
+import { CatDetailsService } from "./../cat-details.service";
+import { ActivatedRoute } from "@angular/router";
+import { Component, Input, OnInit } from "@angular/core";
+import { Location } from "@angular/common/";
 
 @Component({
-  selector: 'app-cat-bio',
-  templateUrl: './cat-bio.component.html',
-  styleUrls: ['./cat-bio.component.css'],
+	selector: "app-cat-bio",
+	templateUrl: "./cat-bio.component.html",
+	styleUrls: ["./cat-bio.component.css"],
 })
 export class CatBioComponent implements OnInit {
-  catBio: Cat;
-  catId: number;
-  constructor(
-    private route: ActivatedRoute,
-    private catDetailsServ: CatDetailsService
-  ) {}
+	catBio: Cat;
+	catId: number;
+	constructor(
+		private route: ActivatedRoute,
+		private catDetailsServ: CatDetailsService,
+		private location: Location
+	) {}
 
-  ngOnInit(): void {
-    this.getCat();
-    this.getCatBio(this.catId);
-  }
+	ngOnInit(): void {
+		this.getCat();
+		this.getCatBio(this.catId);
+	}
 
-  getCat = () => {
-    this.route.queryParamMap.subscribe((params) => {
-      console.log(params.get('cat'));
-      this.catId = parseInt(params.get('cat'));
-    });
-  };
+	getCat = () => {
+		this.route.queryParamMap.subscribe((params) => {
+			//console.log(params.get("cat"));
+			this.catId = parseInt(params.get("cat"));
+		});
+	};
 
-  getCatBio = (catId: number): any => {
-    this.catDetailsServ.getCatForId(catId).subscribe((catIdResponse) => {
-      this.catBio = catIdResponse;
-      console.log('cat id response', this.catBio);
-    });
-  };
+	getCatBio = (catId: number): any => {
+		this.catDetailsServ.getCatForId(catId).subscribe((catIdResponse) => {
+			this.catBio = catIdResponse;
+		});
+	};
+
+	goBackPage = () => {
+		this.location.back();
+	};
 }
